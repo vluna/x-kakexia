95f9b357d47d1a6cbf4a916905a244fdc38791b0
vluna <vluna.0293@gmail.com>
2016-09-18 03:34:03 -0700

    Added data.json with info from sensors to pass to front end

--- js/arduino-script.js
+++ js/arduino-script.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var five = require("johnny-five");
-var force_sensor;
+var force_sensor, force_sensor_2;
+var force_applied, force_applied_2;
 
 (new five.Board()).on("ready", function() {
 
@@ -9,22 +10,55 @@ var force_sensor;
     	freq: 25
   	});
 
-  	window.setInterval(function(){
+  	force_sensor_2 = new five.Sensor({
+  		pin: "A1",
+    	freq: 25
+  	});
+
+  	//setTimeout(function(){
   		var date = new Date();
   		var month = date.getMonth();
   		var day = date.getDay();
   		var year = date.getYear();
 
   		force_sensor.scale([0, 255]).on("data", function() {
-  			var obj = {
+
+
+  			force_applied = this.scaled;
+    		console.log("Force 1: " + this.scaled);
+  		});
+
+
+  		force_sensor_2.scale([0, 255]).on("data", function() {
+  	// 		var obj = {
+			// 	Date: day + ' - ' + month + ' - ' + year,
+			// 	Weight: this.scaled
+			// };
+
+			// var json = JSON.stringify(obj);
+			// fs.writeFile('../data.json', json, 'utf8', function(err) {
+			//     if (err) throw err;
+			//     console.log('complete');
+			//     });
+
+  			force_applied_2 = this.scaled;
+    		console.log("Force 2: " + this.scaled);
+    		var average = (force_applied + force_applied_2) / 2;
+
+    		  	var obj = {
 				Date: day + ' - ' + month + ' - ' + year,
-				Weight: this.scaled
+				Weight: average
 			};
 
 			var json = JSON.stringify(obj);
-			fs.writeFile('../data.json', json, 'utf8', callback);
+			fs.writeFile('../data.json', json, 'utf8', function(err) {
+			    if (err) throw err;
+			    console.log('complete');
+			    });
 
-    		console.log(this.scaled);
+
   		});
-  	}, 60000);
+
+  		//console.log("Average: " + (force_applied + force_applied_2) / 2);
+  	//}, 1000);
 });
--- js/script.js
+++ js/script.js
@@ -1,129 +1,123 @@
-// var five = require("johnny-five"),
-//   fsr;
-
-// (new five.Board()).on("ready", function() {
-
-//   // Create a new `fsr` hardware instance.
-//   fsr = new five.Sensor({
-//     pin: "A0",
-//     freq: 25
-//   });
-
-
-//   // Scale the sensor's value to the LED's brightness range
-//   fsr.scale([0, 255]).on("data", function() {
-
-//     // set the led's brightness based on force
-//     // applied to force sensitive resistor
-//     console.log(this.scaled);
-//   });
-// });
-
+var total_weight;
 
 var opts = {
-  lines: 12, // The number of lines to draw
-  angle: 0, // The length of each line
-  lineWidth: 0.15, // The line thickness
-  pointer: {
-    length: 0.8, // The radius of the inner circle
-    strokeWidth: 0.035, // The rotation offset
-    color: '#000000' // Fill color
-  },
-  limitMax: 'false',   // If true, the pointer will not go past the end of the gauge
-  colorStart: '#6FADCF',   // Colors
-  colorStop: '#8FC0DA',    // just experiment with them
-  strokeColor: '#E0E0E0',   // to see which ones work best for you
-  generateGradient: true
+    lines: 12, // The number of lines to draw
+    angle: 0, // The length of each line
+    lineWidth: 0.15, // The line thickness
+    pointer: {
+        length: 0.8, // The radius of the inner circle
+        strokeWidth: 0.035, // The rotation offset
+        color: '#000000' // Fill color
+    },
+    limitMax: 'true',   // If true, the pointer will not go past the end of the gauge
+    colorStart: '#6FADCF',   // Colors
+    colorStop: '#8FC0DA',    // just experiment with them
+    strokeColor: '#E0E0E0',   // to see which ones work best for you
+    generateGradient: true
 };
 
 var target = document.getElementById('weight'); // your canvas element
 var gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
-gauge.maxValue = 3000; // set max gauge value
+gauge.maxValue = 300; // set max gauge value
 gauge.animationSpeed = 32; // set animation speed (32 is default value)
-gauge.set(1250); // set actual value
+gauge.set(1);
 
-
-// set the dimensions of the canvas
-var margin = {top: 20, right: 20, bottom: 70, left: 40},
-    width = 600 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom;
-
-
-// set the ranges
-var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
-
-var y = d3.scale.linear().range([height, 0]);
-
-// define the axis
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom")
-
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .ticks(10);
-
-
-// add the SVG element
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", 
-          "translate(" + margin.left + "," + margin.top + ")");
-
-
-// load the data
-d3.json("data.json", function(error, data) {
-
-    data.forEach(function(d) {
-        d.Letter = d.Letter;
-        d.Freq = +d.Freq;
+setInterval(function() {
+    $.getJSON('../data.json', function(data) {
+        total_weight = data.Weight.toFixed(2)/2;
+        gauge.animationSpeed = 1;
+        if(total_weight < 1) {
+            gauge.set(1); // set actual value
+            $('.current-weight').html('0(Kg)');
+        } else {
+            gauge.set(total_weight); // set actual value
+            $('.current-weight').html(total_weight+ '(Kg)');
+        }
     });
+}, 500);
+
+
+
+// // set the dimensions of the canvas
+// var margin = {top: 20, right: 20, bottom: 70, left: 40},
+//     width = 600 - margin.left - margin.right,
+//     height = 300 - margin.top - margin.bottom;
+
+
+// // set the ranges
+// var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+
+// var y = d3.scale.linear().range([height, 0]);
+
+// // define the axis
+// var xAxis = d3.svg.axis()
+//     .scale(x)
+//     .orient("bottom")
+
+
+// var yAxis = d3.svg.axis()
+//     .scale(y)
+//     .orient("left")
+//     .ticks(10);
+
+
+// // add the SVG element
+// var svg = d3.select("body").append("svg")
+//     .attr("width", width + margin.left + margin.right)
+//     .attr("height", height + margin.top + margin.bottom)
+//   .append("g")
+//     .attr("transform", 
+//           "translate(" + margin.left + "," + margin.top + ")");
+
+
+// // load the data
+// d3.json("data.json", function(error, data) {
+
+//     data.forEach(function(d) {
+//         d.Letter = d.Letter;
+//         d.Freq = +d.Freq;
+//     });
   
-  // scale the range of the data
-  x.domain(data.map(function(d) { return d.Letter; }));
-  y.domain([0, d3.max(data, function(d) { return d.Freq; })]);
+//   // scale the range of the data
+//   x.domain(data.map(function(d) { return d.Letter; }));
+//   y.domain([0, d3.max(data, function(d) { return d.Freq; })]);
 
-  // add axis
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    .selectAll("text")
-      .style("text-anchor", "end")
-      .attr("dx", "-.8em")
-      .attr("dy", "-.55em")
-      .attr("transform", "rotate(-90)" );
+//   // add axis
+//   svg.append("g")
+//       .attr("class", "x axis")
+//       .attr("transform", "translate(0," + height + ")")
+//       .call(xAxis)
+//     .selectAll("text")
+//       .style("text-anchor", "end")
+//       .attr("dx", "-.8em")
+//       .attr("dy", "-.55em")
+//       .attr("transform", "rotate(-90)" );
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 5)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Frequency");
-
-
-  // Add bar chart
-  svg.selectAll("bar")
-      .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.Letter); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.Freq); })
-      .attr("height", function(d) { return height - y(d.Freq); });
-
-});
+//   svg.append("g")
+//       .attr("class", "y axis")
+//       .call(yAxis)
+//     .append("text")
+//       .attr("transform", "rotate(-90)")
+//       .attr("y", 5)
+//       .attr("dy", ".71em")
+//       .style("text-anchor", "end")
+//       .text("Frequency");
 
 
+//   // Add bar chart
+//   svg.selectAll("bar")
+//       .data(data)
+//     .enter().append("rect")
+//       .attr("class", "bar")
+//       .attr("x", function(d) { return x(d.Letter); })
+//       .attr("width", x.rangeBand())
+//       .attr("y", function(d) { return y(d.Freq); })
+//       .attr("height", function(d) { return height - y(d.Freq); });
 
-    
+// });
+
+
+
 
 $('#menu-questionaire').click(function(){
     $('#chat').css("display", "none");
